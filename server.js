5c7a9563329f1a0cc2afa77964f4c3fc3b5e9a51
ebn646 <ebn646@hotmail.com>
2016-05-22 19:07:57 -0400

    added index.html

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+var http = require('http');
+var path = require("path");
 var express = require('express');
+var bodyParser = require("body-parser");
 
 var app = express();
+app.use(express.static(__dirname + "/public"));
 
 // set up handlebars view engine
 var handlebars = require('express3-handlebars')
@@ -9,6 +13,9 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 5000);
+
+// database configuration
+var mongoose = require('mongoose');
 
 app.use(function(req,res){
     res.type('text/plain');
@@ -22,6 +29,7 @@ app.use(function(err,req,res,next){
     res.status(500);
     res.send('500 - Server Error');
 });
+
 app.listen(app.get('port'),function(){
     console.log('Express started ')
 });
