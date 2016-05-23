b6be38c4388e25330dcb46ff81194b253ee2bf6b
ebn646 <ebn646@hotmail.com>
2016-05-22 20:30:03 -0400

    added angular js

--- public/components/services/BoxOfficeService.js
+++ public/components/services/BoxOfficeService.js
@@ -1,6 +1,6 @@
 angular.module('movieSearchApp')
-    .factory('BoxOfficeService', function ($log, $resource) {
-    var url = 'http://api.themoviedb.org/3/:method/:what';
+    .factory('BoxOfficeService', function ($log, $resource,$http) {
+    var url = '//api.themoviedb.org/3/:method/:what';
     var apikey = 'a47daf25c6cd4e3f68c4ebee27270542';
 
     return $resource(url,
